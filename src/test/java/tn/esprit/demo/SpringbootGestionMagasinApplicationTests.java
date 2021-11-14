4078682264bcb881a377d93671e2d7da60c534b5
MahdiBehi <78915698+MahdiBehiEsprit@users.noreply.github.com>
2021-11-14 21:13:21 +0100

    tested lmethod1

--- src/test/java/tn/esprit/demo/SpringbootGestionMagasinApplicationTests.java
+++ src/test/java/tn/esprit/demo/SpringbootGestionMagasinApplicationTests.java
@@ -1,17 +1,58 @@
 package tn.esprit.demo;
 
-import org.junit.jupiter.api.Test;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 import tn.esprit.demo.entities.Client;
+import tn.esprit.demo.entities.Fournisseur;
+import tn.esprit.demo.entities.Produit;
 import tn.esprit.demo.service.ClientService;
+import tn.esprit.demo.service.FournisseurService;
+import tn.esprit.demo.service.ProduitService;
 
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.skyscreamer.jsonassert.JSONAssert.assertEquals;
-
+@Slf4j
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class SpringbootGestionMagasinApplicationTests {
+public class SpringbootGestionMagasinApplicationTests {
+
+    @Autowired
+    ProduitService pr;
+    @Autowired
+    FournisseurService fr;
+    @Transactional
+    @org.junit.Test
+    public void TestAssignStock(){
+        boolean b = false ;
+        pr.assignProduitToStock(1L,2L);
+        Produit p = pr.retrieveProduit(1L);
+        if (p.getStock().getIdStock() == 2L ){
+            b= true;
+        }
+        assertTrue(b);
+    }
+
+    /*
+    @Transactional
+    @Test
+    public void TestAssignFournisseur(){
+        boolean b = false ;
+        pr.assignFournisseurToProduit(2L,1L);
+        Produit p = pr.retrieveProduit(1L);
+        Fournisseur f = fr.retrieveFournisseur(2L);
+        log.warn(f.getLibelle());
+        if (p.getFournisseur().contains(f)  ){
+            b= true;
+        }
+        assertTrue(b);
+    }
 
 	/*
 	@Autowired
